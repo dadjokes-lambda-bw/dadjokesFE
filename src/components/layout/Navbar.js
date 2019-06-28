18c91c47f2b40b0f02c94f658945e682dad5dbf5
UnknownMonk <smodcartoon@gmail.com>
2019-06-27 20:55:24 -0400

    search bar style

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,53 +1,43 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import Login from '../Login'
-import Registration from '../Registration'
-import './Navbar.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Login from '../Login';
+import Registration from '../Registration';
+import './Navbar.css';
 
 // import SearchForm from '../search/SearchForm'
 
-
-
 const divStyle = {
-    fontSize: '3em'
-}
-
+  fontSize: '3em'
+};
 
 const Navbar = () => {
-    
-    return (
-        <div className="nav">
-        <input type="checkbox" id="nav-check"/>
-        <div className="nav-header">
-          <div className="nav-title">
-            DAD JOKES
-          </div>
-        </div>
-        <div className="nav-btn">
-          <label for="nav-check">
-            <span></span>
-            <span></span>
-            <span></span>
-          </label>
-        </div>
-        <NavLink className="nav-links" to='/'>Home</NavLink>
-        <NavLink className="nav-links" to='/signin'>Sign In</NavLink>
-        <NavLink className="nav-links" to='/signup'>Sign Up</NavLink>
-        <NavLink className="nav-links" to='/addjokes'>Add Jokes</NavLink>
-        <NavLink className="nav-links" to='/searchjokes'>Search Jokes</NavLink>
-        </div>
-       
-        
-    
-    
-    
-
-    )
-}
-
-           
-
-
-
+  return (
+    <div className="nav">
+      <input type="checkbox" id="nav-check" />
+      <div className="nav-header">
+        <div className="nav-title">DAD JOKES</div>
+      </div>
+      <div className="nav-btn">
+        <label for="nav-check">
+          <span />
+          <span />
+          <span />
+        </label>
+      </div>
+      <NavLink className="nav-links" to="/">
+        Home
+      </NavLink>
+      <NavLink className="nav-links" to="/signin">
+        Sign In
+      </NavLink>
+      <NavLink className="nav-links" to="/signup">
+        Sign Up
+      </NavLink>
+      <NavLink className="nav-links" to="/addjokes">
+        Add Jokes
+      </NavLink>
+    </div>
+  );
+};
 
 export default Navbar;
